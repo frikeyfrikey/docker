cbf0550052d8b0942f213e56963799c519b56ca7
frikeyfrikey <95304112+frikeyfrikey@users.noreply.github.com>
2024-01-18 00:04:45 +0300

    Update python.py

--- python.py
+++ python.py
@@ -6,4 +6,39 @@ zapi = ZabbixAPI(ZABBIX_SERVER)
 
 zapi.login("api_username", "api_password")
 
-trigger=zapi.trigger.get()
+# Get a list of all issues (AKA tripped triggers)
+triggers = zapi.trigger.get(
+    only_true=1,
+    skipDependent=1,
+    monitored=1,
+    active=1,
+    output="extend",
+    expandDescription=1,
+    selectHosts=["host"],
+)
+
+# Do another query to find out which issues are Unacknowledged
+unack_triggers = zapi.trigger.get(
+    only_true=1,
+    skipDependent=1,
+    monitored=1,
+    active=1,
+    output="extend",
+    expandDescription=1,
+    selectHosts=["host"],
+    withLastEventUnacknowledged=1,
+)
+unack_trigger_ids = [t["triggerid"] for t in unack_triggers]
+for t in triggers:
+    t["unacknowledged"] = True if t["triggerid"] in unack_trigger_ids else False
+
+# Print a list containing only "tripped" triggers
+for t in triggers:
+    if int(t["value"]) == 1:
+        print(
+            "{} - {} {}".format(
+                t["hosts"][0]["host"],
+                t["description"],
+                "(Unack)" if t["unacknowledged"] else "",
+            )
+        )
