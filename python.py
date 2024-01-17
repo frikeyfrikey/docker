e131197f5b163ccc4be222c67ae3b764fcb15f4b
frikeyfrikey <95304112+frikeyfrikey@users.noreply.github.com>
2024-01-17 23:58:46 +0300

    Update python.py

--- python.py
+++ python.py
@@ -6,12 +6,4 @@ zapi = ZabbixAPI(ZABBIX_SERVER)
 
 zapi.login("api_username", "api_password")
 
-triggers = zapi.trigger.get(
-    only_true=1,
-    skipDependent=1,
-    monitored=1,
-    active=1,
-    output="extend",
-    expandDescription=1,
-    selectHosts=["host"],
-)
+trigger=zapi.trigger.get()
